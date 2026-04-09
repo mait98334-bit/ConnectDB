64175b953af95b8f8d3662373ad8e07a86b8fc9d
TrangSi <mait98334@gmail.com>
2026-04-09 11:28:48 +0700

    Thiết lập Data Annotations cho thực thể Student theo yêu cầu

--- Models/Student.cs
+++ Models/Student.cs
@@ -1,15 +1,20 @@
 using System.ComponentModel.DataAnnotations;
-namespace ConnectDB.Models;
-public class Student
-{
-    [Key]
-    public int Id { get; set; }
-    [Required]
-    [StringLength(20)]
-    public string StudentCode { get; set; } = string.Empty;
-    [Required]
-    [StringLength(100)]
-    public string FullName { get; set; } = string.Empty;
-    public DateTime Birthday { get; set; }
-}
 
+namespace ConnectDB.Models
+{
+    public class Student
+    {
+        [Key]
+        public int Id { get; set; }
+
+        [Required] 
+        [StringLength(20)] 
+        public string StudentCode { get; set; }
+
+        [Required] 
+        [StringLength(100)] 
+        public string FullName { get; set; }
+
+        public DateTime Birthday { get; set; }
+    }
+}
